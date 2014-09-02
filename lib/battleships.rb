ef86ff4cf681014dba634e8e6c78cb081eab634c
Henry Stanley <henry@henrystanley.com>
2014-09-02 14:18:54 +0100

    tests passing

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,6 +5,14 @@ class Battleships < Sinatra::Base
     erb :index
   end
 
+  get '/newplayer' do
+  	erb :newplayer
+  end
+
+  get '/hi' do
+  	"Hello!"
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
