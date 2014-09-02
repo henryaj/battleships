ee04a9ca1561536d2b0c6f95af9a371fdc3dedf2
Henry Stanley <henry@henrystanley.com>
2014-09-02 17:49:58 +0100

    Added pending Cucumber examples of player initialisation

--- features/starting_a_game.feature
+++ features/starting_a_game.feature
@@ -9,7 +9,7 @@ Feature: Starting the game
 		Then I should see "What's your name?"
 
 	Scenario: Registering
-		Given I am on the newplayer page
+		Given I am on the page 'newplayer'
 		When I type in my name
 		Then I should see "Hello, " name
 		Then my name should be passed to player1.name
---
+++ features/step_definitions/starting_a_game_steps.rb
@@ -0,0 +1,11 @@
+When(/^I type in my name$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" name$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my name should be passed to player(\d+)\.name$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
